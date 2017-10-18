ecd4038a7c8141a394e2618668dd52949a0310f8
yommmm <1529133840@qq.com>
2017-10-18 23:19:41 +0800

    add junit

---
+++ security-demo/src/test/java/controller/DemoApplicationTest.java
@@ -0,0 +1,65 @@
+package controller;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTest {
+
+	@Autowired
+	private WebApplicationContext context;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
+	
+	@Test
+	public void whenQuerySuccess() throws Exception {
+		String result = mockMvc.perform(
+				get("/user").param("username", "jojo").param("age", "18").param("ageTo", "60").param("xxx", "yyy")
+						// .param("size", "15")
+						// .param("page", "3")
+						// .param("sort", "age,desc")
+						.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk()).andExpect(jsonPath("$.length()").value(3))
+				.andReturn().getResponse().getContentAsString();
+		
+		System.out.println(result);
+	}
+	
+	@Test
+	public void whenGetInfoSuccess() throws Exception {
+		String result = mockMvc.perform(get("/user/1")
+				.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.username").value("tom"))
+				.andReturn().getResponse().getContentAsString();
+		
+		System.out.println(result);
+	}
+	
+	@Test
+	public void whenGetInfoFail() throws Exception {
+		mockMvc.perform(get("/user/a")
+				.contentType(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().is4xxClientError());
+	}
+
+}
