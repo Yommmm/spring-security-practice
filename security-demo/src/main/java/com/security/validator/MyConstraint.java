9a020002f6bd94ae1a775773b639910f1deabb9c
yommmm <1529133840@qq.com>
2017-11-11 18:41:12 +0800

    add an annotion

---
+++ security-demo/src/main/java/com/security/validator/MyConstraint.java
@@ -0,0 +1,15 @@
+package com.security.validator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = )
+public @interface MyConstraint {
+
+}
