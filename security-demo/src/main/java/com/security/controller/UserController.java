f75d967fca8f5b846be97e34ab27c29d898568ce
yommmm <1529133840@qq.com>
2017-11-16 22:43:13 +0800

    add UserNotExistException

--- security-demo/src/main/java/com/security/controller/UserController.java
+++ security-demo/src/main/java/com/security/controller/UserController.java
@@ -10,6 +10,7 @@ import org.apache.commons.lang.builder.ToStringStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,9 +45,10 @@ public class UserController {
 	@GetMapping(value = "/{id:\\d+}")
 	@JsonView(User.UserDetailView.class)
 	public User getInfo(@PathVariable(value = "id") String id) {
-		User user = new User();
-		user.setUsername("tom");
-		return user;
+		throw new UsernameNotFoundException(id);
+//		User user = new User();
+//		user.setUsername("tom");
+//		return user;
 	}
 
 	@PostMapping
