e170dcb892201de690a3c66d555533b2d2ab1860
yommmm <1529133840@qq.com>
2017-11-16 23:06:28 +0800

    add ExceptionHandler

---
+++ security-demo/src/main/java/com/security/exception/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.security.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(UserNotExistException.class)
+	@ResponseBody
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public Map<String, Object> handlerUserNotExistException(UserNotExistException ex){
+		Map<String, Object> result = new HashMap<String, Object>();
+		result.put("id", ex.getId());
+		result.put("message", ex.getMessage());
+		return result;
+	}
+}
