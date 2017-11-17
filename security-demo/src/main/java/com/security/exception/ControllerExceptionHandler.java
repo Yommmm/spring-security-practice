133aae3fe1c81f7b7540bbee94e985e9e44d9968
yommmm <1529133840@qq.com>
2017-11-17 22:05:15 +0800

    add code

--- security-demo/src/main/java/com/security/exception/ControllerExceptionHandler.java
+++ security-demo/src/main/java/com/security/exception/ControllerExceptionHandler.java
@@ -3,6 +3,8 @@ package com.security.exception;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+	private static final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+	
 	@ExceptionHandler(UserNotExistException.class)
 	@ResponseBody
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -19,6 +23,7 @@ public class ControllerExceptionHandler {
 		Map<String, Object> result = new HashMap<String, Object>();
 		result.put("id", ex.getId());
 		result.put("message", ex.getMessage());
+		logger.info("================{}",ex.getId(),"++++++++++++++{}",ex.getMessage());
 		return result;
 	}
 }
