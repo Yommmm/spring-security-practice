4e8a57e50f57c861984222642a859701de7d1ba4
yommmm <1529133840@qq.com>
2017-11-18 23:37:02 +0800

    learning interceptor

--- security-demo/src/main/java/com/security/web/config/WebConfig.java
+++ security-demo/src/main/java/com/security/web/config/WebConfig.java
@@ -3,15 +3,22 @@ package com.security.web.config;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.security.web.filter.TimerFilter;
+import com.security.web.interceptor.TimerInterceptor;
 
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
+	@Autowired
+	private TimerInterceptor timerInterceptor;
+	
 	@Bean
 	public FilterRegistrationBean timeFilter(){
 		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
@@ -25,4 +32,10 @@ public class WebConfig {
 		
 		return registrationBean;
 	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(timerInterceptor);
+	}
+	
 }
--- security-demo/src/main/java/com/security/web/interceptor/TimerInterceptor.java
+++ security-demo/src/main/java/com/security/web/interceptor/TimerInterceptor.java
@@ -7,33 +7,48 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 
+ * @author Yom
+ *
+ */
+@Component
 public class TimerInterceptor implements HandlerInterceptor {
 
 	private static final Logger logger = LoggerFactory.getLogger(TimerInterceptor.class);
 	
 	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object hander, Exception ex)
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-		logger.info("============afterCompletion============");
+		logger.debug("============afterCompletion============");
+		long start = (long)request.getAttribute("startTime");
+		logger.info("============finish in =============={}", new Date().getTime() - start);
+		System.out.println(ex);
 	}
 
 	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object hander, ModelAndView model)
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView model)
 			throws Exception {
-		logger.info("============postHandler=============");
-		
-		logger.info("============start time =============={}",new Date().getTime());
+		logger.debug("============postHandler=============");
+		long start = (long)request.getAttribute("startTime");
+		logger.info("============finish in =============={}", new Date().getTime() - start);
 	}
 
 	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse reponse, Object hander) throws Exception {
-		logger.info("=============preHandler============");
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse reponse, Object handler) throws Exception {
+		logger.debug("=============preHandler============");
 		logger.info("=============start time ==========={}", new Date().getTime());
+		
+		logger.info(((HandlerMethod)handler).getBean().getClass().getName());
+		logger.info(((HandlerMethod)handler).getMethod().getName());
+		
 		request.setAttribute("startTime", new Date().getTime());
-		return false;
+		return true;
 	}
 
 }
