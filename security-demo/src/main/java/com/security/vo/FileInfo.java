1fca390096a6c51a480a79bf4d7137ac2db4df37
yommmm <1529133840@qq.com>
2017-11-20 00:00:04 +0800

    add

---
+++ security-demo/src/main/java/com/security/vo/FileInfo.java
@@ -0,0 +1,20 @@
+package com.security.vo;
+
+public class FileInfo {
+
+	private String path;
+
+	public FileInfo(String path) {
+		this.path = path;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+}
--- security-demo/src/main/java/com/security/web/controller/FileController.java
+++ security-demo/src/main/java/com/security/web/controller/FileController.java
@@ -1,13 +1,35 @@
 package com.security.web.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.security.vo.FileInfo;
 
 @RestController
 @RequestMapping("/file")
 public class FileController {
 
-	private String folder = "";
+	private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 	
+	private String folder = "F://";
 	
+	@PostMapping
+	public FileInfo upload(MultipartFile file) throws IllegalStateException, IOException{
+		logger.info(file.getName());
+		logger.info(file.getOriginalFilename());
+		System.out.println(file.getSize());
+		
+		File localFile = new File(folder, new Date().getTime() + ".txt");
+		file.transferTo(localFile);
+		
+		return new FileInfo(localFile.getAbsolutePath());
+	}
 }
