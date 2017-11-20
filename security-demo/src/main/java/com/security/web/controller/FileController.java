575df8e8e98ab7e7d805c3af4a5b8a3c6927470f
yommmm <1529133840@qq.com>
2017-11-20 23:20:41 +0800

    add download interface

--- security-demo/src/main/java/com/security/web/controller/FileController.java
+++ security-demo/src/main/java/com/security/web/controller/FileController.java
@@ -1,11 +1,21 @@
 package com.security.web.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,4 +42,20 @@ public class FileController {
 		
 		return new FileInfo(localFile.getAbsolutePath());
 	}
+	
+	@GetMapping("/{id}")
+	public void download(@PathVariable String id, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		// 
+		try (InputStream inputStream = new FileInputStream(new File(folder, id + ".txt"));
+				OutputStream outputStream = response.getOutputStream();) {
+			
+			response.setContentType("application/x-download");
+			response.addHeader("Content-Disposition", "attachment;filename=test.txt");
+			
+			IOUtils.copy(inputStream, outputStream);
+			outputStream.flush();
+		}
+	}
+	
 }
