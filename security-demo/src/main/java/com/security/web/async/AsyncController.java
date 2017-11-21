60e2aea91b01454869de88b5283e78090628125e
yommmm <1529133840@qq.com>
2017-11-21 23:12:54 +0800

    add

--- security-demo/src/main/java/com/security/web/async/AsyncController.java
+++ security-demo/src/main/java/com/security/web/async/AsyncController.java
@@ -1,5 +1,13 @@
 package com.security.web.async;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AsyncController {
 
+	private static final Logger logger = LoggerFactory.getLogger(AsyncController.class);
+	
+	
 }
